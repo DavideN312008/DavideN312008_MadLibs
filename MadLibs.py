d1c7fc4c7572f0773e3d93b8fd3953fe4ec9e8a3
Davide <98412272+DavideN312008@users.noreply.github.com>
2022-01-26 18:36:25 +0200

    Add files via upload

---
+++ MadLibs.py
@@ -0,0 +1,35 @@
+loop = 1 
+
+while (loop < 10 ):
+
+    relative1 = input("Choose a relative: ")
+    adjective1 = input("Choose an adjective: ")
+    adjective2 = input("Choose an adjective: ")
+    adjective3 = input("Choose an adjective: ")
+    name_of_person_in_room = input("Choose the name of person in the room: ")
+    adjective4 = input("Choose an adjective: ")
+    adjective5 = input("Choose an adjective: ")
+    verb_ending_in_ed = input("Choose a verb ending in ''ed'': ")
+    body_part = input("Choose a body part: ")
+    verb_ending_in_ing = input("Choose a verb ending in ''ing'': ")
+    adjective = input("Choose an adjective: ")
+    plural_noun = input("Choose a plural noun: ")
+    noun = input("Choose a noun: ")
+    adverb = input("Choose an adverb: ")
+    verb1 = input("Choose a verb: ")
+    verb2 = input("Choose a verb: ")
+    relative2 = input("Choose a relative: ")
+    person_in_room = input("Choose the persone in the room: ")
+
+    print("------------------------------------------")
+    print("Dear",relative1,".")
+    print("I am having a(n)", adjective1,"time at the camp.")
+    print("The counselour is", adjective2, "and the food is", adjective3,".")
+    print("I met",name_of_person_in_room,"and we became", adjective4,"friends.")
+    print("Unfortunately,",name_of_person_in_room,"is", adjective5,"and I",verb_ending_in_ed,"my",body_part,"so we couldn't go",verb_ending_in_ing,"like everybody else.")
+    print("I need more",plural_noun,"and a",noun,"sharpener, so please",adverb,verb1,"more when you",verb2,"back.")
+    print("Your",relative2,".")
+    print (person_in_room)
+    print("------------------------------------------")
+
+    loop = loop + 1
